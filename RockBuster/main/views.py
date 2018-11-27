26dedf24e3f29e8f19f965ec9b73ef9f81022275
SethW <sw2401@gmail.com>
2018-11-27 12:38:39 -0500

    add html css js files

--- RockBuster/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello, Welcome to RockBuster</h1>"+
-                        "<h2>Taste the Dwayne</h2>")
